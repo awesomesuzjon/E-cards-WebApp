790f7b5573d3d146ff20a57c360bb7cf18185fb5
Sujan Bohara <bohorasujan39@gmail.com>
2022-06-01 21:24:06 +0545

    carousel added

--- src/lib/addToCanvas/stickers.svelte
+++ src/lib/addToCanvas/stickers.svelte
@@ -1,15 +1,142 @@
-<div id="drag-items" class="m-2 p-4 flex flex-col gap-4" >
-	<!-- svelte-ignore a11y-missing-attribute -->
-	<img class="w-24" src="https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=100&q=80" draggable="true" />
-	<!-- svelte-ignore a11y-missing-attribute -->
-	<img class="w-24" src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoYW5nZXxlbnwwfHwwfHw%3D&w=100&q=80" draggable="true" />
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<img class="w-24" src="dimag.jpg" draggable="true" />
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<img class="w-24" src="hand.jpg" draggable="true" />
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<img class="w-24" src="namaste.jpg" draggable="true" />
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<img class="w-24" src="ktm.jpg" draggable="true" />
-	
-  </div>
+<div id="controls-carousel" class="relative" data-carousel="static">
+	<div id="drag-items">
+		<div class="overflow-hidden relative h-48 rounded-lg sm:h-64 xl:h-80 2xl:h-64">
+			<div
+				class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
+				data-carousel-item="active"
+			>
+				<img
+					src="hand.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+
+				<img
+					src="ktm.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+				<img
+					src="admin.png"
+					class="rounded-full w-16 block absolute top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+			</div>
+
+			<div
+				class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
+				data-carousel-item="active"
+			>
+				<img
+					src="hand.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+
+				<img
+					src="ktm.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+				<img
+					src="admin.png"
+					class="rounded-full w-16 block absolute top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+			</div>
+
+			<div
+				class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
+				data-carousel-item="active"
+			>
+				<img
+					src="dimag.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+
+				<img
+					src="hand.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+				<img
+					src="admin.png"
+					class="rounded-full w-16 block absolute top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+			</div>
+
+			<div
+				class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
+				data-carousel-item="active"
+			>
+				<img
+					src="ktm.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+
+				<img
+					src="hand.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+				<img
+					src="hand.jpg"
+					class="rounded-full w-16 block absolute top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2"
+					alt="..."
+				/>
+			</div>
+		</div>
+
+		<button
+			type="button"
+			class="flex absolute top-0 left-0 z-30 justify-center items-center px-1 h-full cursor-pointer group focus:outline-none"
+			data-carousel-prev=""
+		>
+			<span
+				class="inline-flex justify-center items-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+			>
+				<svg
+					class="w-6 h-6 text-white dark:text-gray-800"
+					fill="none"
+					stroke="currentColor"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M15 19l-7-7 7-7"
+					/></svg
+				>
+				<span class="hidden">Previous</span>
+			</span>
+		</button>
+		<button
+			type="button"
+			class="flex absolute top-0 right-0 z-30 justify-center items-center px-1 h-full cursor-pointer group focus:outline-none"
+			data-carousel-next=""
+		>
+			<span
+				class="inline-flex justify-center items-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+			>
+				<svg
+					class="w-6 h-6 text-white dark:text-gray-800"
+					fill="none"
+					stroke="currentColor"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M9 5l7 7-7 7"
+					/></svg
+				>
+				<span class="hidden">Next</span>
+			</span>
+		</button>
+	</div>
+</div>
