cbb1d0af972bafc8f9bf620ce7645a26f7b2359e
mukezhz <mukezhz@gmail.com>
2022-07-15 08:36:18 +0545

    feat: adds sveltejs-node adaptor for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	// existing props
-	kit: {
-		vite: { optimizeDeps: { include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep'] } }
-	  }
-	}
+  const config = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
+    kit: {
+      adapter:adapter(),
+      // existing props
+      vite: { optimizeDeps: { include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep'] } }
+    } 
+  }
 
 
 export default config;
